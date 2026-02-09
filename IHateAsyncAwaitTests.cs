f8dc3db757751fc5cf75f0e74263b390bbbf12d5
antonlyzinpropup <114113114+antonlyzinpropup@users.noreply.github.com>
2026-02-09 10:04:13 -0800

    Rename and enhance async test methods

--- IHateAsyncAwaitTests.cs
+++ IHateAsyncAwaitTests.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using System.Threading;
+using Nito.AsyncEx.Synchronous;
 
 namespace BusinessRules.Tests
 {
@@ -10,29 +13,45 @@ namespace BusinessRules.Tests
     public class IHateAsyncAwaitTests
     {
         [TestMethod]
-        public void AsyncVsRegularCallTest()
+        public void AsyncVsRegularSyntheticCallTest()
         {
-            //A test to compare a generic code with async/await VS proper task usage, vs sync mode
-            //The test is measuring 100k requests like async Controller::method -> calls async "Service"::method -> calls few other "async" and await some long-running operations (like db, IO etc)
             var sw = new Stopwatch();
 
-            sw.Start();
-            Parallel.For(0, 100 * 1000, (i) =>
+            /*sw.Start();
+            Parallel.For(0, 100 * 1000, new ParallelOptions(){MaxDegreeOfParallelism = 100 * 1000}, (i) =>
             {
-                Task.WaitAll(SimulateSomeWorkAndCallItSelfAsync(150, 10));
+                SimulateSomeWorkAndCallItSelfAsync(150, 10).WaitAndUnwrapException();
             });
 
+            // 1124s
             Console.Out.WriteLine("Execution time async: " + sw.Elapsed.TotalSeconds); sw.Start();
+            */
 
+            /*
             sw.Restart();
 
             Parallel.For(0, 100 * 1000, (i) =>
             {
-                Task.WaitAll(SimulateSomeWorkAndCallItSelfAltAsync(150, 10));
+                SimulateSomeWorkAndCallItSelfAltAsync(150, 10).Wait();
             });
 
+            //567s
             Console.Out.WriteLine("Execution time async2: " + sw.Elapsed.TotalSeconds); sw.Start();
+            */
 
+            /*
+            sw.Restart();
+            Task.WaitAll(Enumerable.Range(0, 100 * 1000).Select(i => SimulateSomeWorkAndCallItSelfAsync(150, 10)).ToArray());
+
+            //2.5s
+            Console.Out.WriteLine("Execution time async (method 2): " + sw.Elapsed.TotalSeconds);
+            */
+
+            sw.Restart();
+            Task.WaitAll(Enumerable.Range(0, 100 * 1000).Select(i => new Task(() => SimulateSomeWorkAndCallItSelf(150, 10))).ToArray());
+
+            Console.Out.WriteLine("Execution time sync (method 2): " + sw.Elapsed.TotalSeconds);
+            
             sw.Restart();
 
             Parallel.For(0, 100 * 1000, (i) =>
@@ -40,20 +59,73 @@ namespace BusinessRules.Tests
                 SimulateSomeWorkAndCallItSelf(150, 10);
             });
 
+            //105s
             Console.Out.WriteLine("Execution time sync: " + sw.Elapsed.TotalSeconds);
+            
 
             //Execution time async: 1124s
             //Execution time async2: 567s
+            //Execution time async method 2: 2.5s
+
+
             //Execution time sync: 105s
         }
 
-        private static async Task SimulateSomeWorkAndCallItSelfAsync(int msDelay, int moreDeepCalls)
+        [TestMethod]
+        public void AsyncVsRegularRealWorkCallTest()
+        {
+            var sw = new Stopwatch();
+
+            sw.Start();
+            Parallel.For(0, 100 * 1000, new ParallelOptions(){MaxDegreeOfParallelism = 100 * 1000}, (i) =>
+            {
+                SimulateSomeRealWorkAndCallItSelfAsync(150, 10).WaitAndUnwrapException();
+            });
+
+            // 985s
+            Console.Out.WriteLine("Execution time async: " + sw.Elapsed.TotalSeconds); sw.Start();
+            
+            sw.Restart();
+
+            Parallel.For(0, 100 * 1000, (i) =>
+            {
+                SimulateSomeRealWorkAndCallItSelfAltAsync(150, 10).Wait();
+            });
+
+            //1616s
+            Console.Out.WriteLine("Execution time async2: " + sw.Elapsed.TotalSeconds); sw.Start();
+            
+            
+            sw.Restart();
+            Task.WaitAll(Enumerable.Range(0, 100 * 1000).Select(i => SimulateSomeRealWorkAndCallItSelfAsync(150, 10)).ToArray<Task>());
+
+            //.NET process consumed over 20gb on 60kb file, test never complete
+            Console.Out.WriteLine("Execution time async (method 2): " + sw.Elapsed.TotalSeconds);
+            
+
+            sw.Restart();
+
+            Parallel.For(0, 100 * 1000, (i) =>
+            {
+                SimulateSomeRealWorkAndCallItSelf(150, 10);
+            });
+
+            //409s
+            Console.Out.WriteLine("Execution time sync: " + sw.Elapsed.TotalSeconds);
+        }
+
+        private static async Task<string[]> SimulateSomeWorkAndCallItSelfAsync(int msDelay, int moreDeepCalls)
         {
             await Task.Delay(TimeSpan.FromMilliseconds(msDelay));
 
             if (moreDeepCalls > 0)
             {
-                await SimulateSomeWorkAndCallItSelfAsync(msDelay, moreDeepCalls - 1);
+                return await SimulateSomeWorkAndCallItSelfAsync(msDelay, moreDeepCalls - 1);
+            }
+            else
+            {
+                //Read file...
+                return await File.ReadAllLinesAsync(@"C:\\temp\\allMetrics.csv");
             }
         }
 
@@ -78,6 +150,45 @@ namespace BusinessRules.Tests
             {
                 SimulateSomeWorkAndCallItSelf(msDelay, moreDeepCalls - 1);
             }
+        }
+
+        private static async Task<string[]> SimulateSomeRealWorkAndCallItSelfAsync(int msDelay, int moreDeepCalls)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(msDelay));
+
+            if (moreDeepCalls > 0)
+            {
+                return await SimulateSomeWorkAndCallItSelfAsync(msDelay, moreDeepCalls - 1);
+            }
+
+            //Read file...
+            return await File.ReadAllLinesAsync(@"C:\\temp\\allMetrics.csv");
+        }
+
+        private static Task<string[]> SimulateSomeRealWorkAndCallItSelfAltAsync(int msDelay, int moreDeepCalls)
+        {
+            if (moreDeepCalls > 0)
+            {
+                Task.Delay(TimeSpan.FromMilliseconds(msDelay)).Wait();
+
+                return SimulateSomeRealWorkAndCallItSelfAltAsync(msDelay, moreDeepCalls - 1);
+            }
+            
+            //Read file...
+            return File.ReadAllLinesAsync(@"C:\\temp\\allMetrics.csv");
+        }
+
+        private static string[] SimulateSomeRealWorkAndCallItSelf(int msDelay, int moreDeepCalls)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(msDelay));
+
+            if (moreDeepCalls > 0)
+            {
+                return SimulateSomeRealWorkAndCallItSelf(msDelay, moreDeepCalls - 1);
+            }
+            
+            //Read file...
+            return File.ReadAllLines(@"C:\\temp\\allMetrics.csv");
         }
     }
 }
